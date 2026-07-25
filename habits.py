992683578da2349991ff6c32be851f2664f7c758
anna4claes <annaclaes@ukr.net>
2026-07-25 19:43:44 +0300

    Create habits.py

---
+++ habits.py
@@ -0,0 +1,28 @@
+HABITS = [
+
+    {
+        "name": "Morning Exercise",
+        "completed": True
+    },
+
+    {
+        "name": "Read 20 Pages",
+        "completed": True
+    },
+
+    {
+        "name": "Drink 2L Water",
+        "completed": False
+    },
+
+    {
+        "name": "Meditation",
+        "completed": True
+    },
+
+    {
+        "name": "Walk 8000 Steps",
+        "completed": False
+    }
+
+]
